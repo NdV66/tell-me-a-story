be07a82c5ea449fda26ffb53274a50fed440e32a
Marta Zazlak <marta.zazlak@hotmail.com>
2023-06-12 10:39:20 +0200

    useStateObservable

---
+++ src/tools/useStateObservable.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { Observable } from 'rxjs';
+
+//TODO: tests
+export const useStateObservable = <T>(observable: Observable<T>, initialValue: T | null) => {
+  const [data, setData] = useState(initialValue);
+
+  useEffect(() => {
+    observable.subscribe(setData);
+  }, [observable]);
+
+  return data;
+};
