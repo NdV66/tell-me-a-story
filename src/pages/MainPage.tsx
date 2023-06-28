6829c6832894dc79d48205b49a0d288ce833d9f8
Marta Zazlak <marta.zazlak@hotmail.com>
2023-06-28 14:49:14 +0200

    tellStoryOnceAgain

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -35,13 +35,6 @@ const useMainPage = () => {
       },
       palette,
       components: {
-        MuiSlider: {
-          styleOverrides: {
-            root: {
-              fontSize: '5px',
-            },
-          },
-        },
         MuiPaper: {
           styleOverrides: {
             root: {
