d26eba46bb53af8b82a30c7452710893120af315
Marta Zazlak <marta.zazlak@hotmail.com>
2023-07-21 21:28:07 +0200

    rtl

--- src/tests/pages/StorySettingsComponent/StoryCategoriesSelector.test.tsx
+++ src/tests/pages/StorySettingsComponent/StoryCategoriesSelector.test.tsx
@@ -1,12 +1,25 @@
 import { act, render, renderHook, screen } from '@testing-library/react';
+import { Theme } from '@mui/material/styles';
+import { SelectChangeEvent } from '@mui/material';
+import { Subject } from 'rxjs';
 
 import { settingsContextValueMock, storyCategoriesViewComponentMock, wrapper } from 'tests/mocks';
-import { useStoryCategoriesSelector } from 'pages/StorySettingsComponent/StoryCategoriesSelector';
-import { Subject } from 'rxjs';
+import {
+  StoryCategoriesSelector,
+  useStoryCategoriesSelector,
+} from 'pages/StorySettingsComponent/StoryCategoriesSelector';
 import { EStoryCategory } from 'types';
+import { SettingContext } from 'context';
 
 const expectedCategories = [EStoryCategory.BOTTLES, EStoryCategory.FOOD];
 const { translations } = settingsContextValueMock;
+
+const checkExpectedCategoriesChips = () => {
+  expectedCategories.forEach((key) => {
+    const text = translations.categoriesByKeys[key];
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+};
 
 describe('StoryCategoriesSelector - useStoryCategoriesSelector', () => {
   let currentCategories$: Subject<EStoryCategory[]>;
@@ -37,7 +50,6 @@ describe('StoryCategoriesSelector - useStoryCategoriesSelector', () => {
 
   test('translateCategoryByKey()', () => {
     const { result } = renderElement();
-
     act(() => currentCategories$.next(expectedCategories));
 
     const data = result.current.translateCategoryByKey(expectedCategories[0]);
@@ -46,12 +58,70 @@ describe('StoryCategoriesSelector - useStoryCategoriesSelector', () => {
 
   test('renderValue()', () => {
     const { result } = renderElement();
-
     render(result.current.renderValue(expectedCategories));
+    checkExpectedCategoriesChips();
+  });
 
-    expectedCategories.forEach((key) => {
-      const text = translations.categoriesByKeys[key];
-      expect(screen.getByText(text)).toBeInTheDocument();
+  test('handleChange()', () => {
+    const expectedValue = EStoryCategory.CREATURES;
+    const { result } = renderElement();
+
+    result.current.handleChange({ target: { value: expectedValue } } as SelectChangeEvent<
+      string[]
+    >);
+
+    expect(storyCategoriesViewComponentMock.changeCategories).toHaveBeenCalled();
+    expect(storyCategoriesViewComponentMock.changeCategories).toHaveBeenCalledTimes(1);
+    expect(storyCategoriesViewComponentMock.changeCategories).toHaveBeenCalledWith(expectedValue);
+  });
+
+  describe('getItemStyles()', () => {
+    test('- the value is not included', () => {
+      const value = EStoryCategory.CREATURES;
+      const expectedTypography = 22;
+      const themeMock = {
+        typography: { fontWeightRegular: expectedTypography },
+      } as Theme;
+      const { result } = renderElement();
+
+      const data = result.current.getItemStyles(value, expectedCategories, themeMock);
+      expect(data).toEqual({ fontWeight: expectedTypography });
     });
+
+    test('- the value is included', () => {
+      const value = expectedCategories[0];
+      const expectedTypography = 22;
+      const themeMock = {
+        typography: { fontWeightBold: expectedTypography },
+      } as Theme;
+      const { result } = renderElement();
+
+      const data = result.current.getItemStyles(value, expectedCategories, themeMock);
+      expect(data).toEqual({ fontWeight: expectedTypography });
+    });
+  });
+});
+
+describe('StoryCategoriesSelector', () => {
+  let currentCategories$: Subject<EStoryCategory[]>;
+
+  beforeEach(() => {
+    currentCategories$ = new Subject();
+    storyCategoriesViewComponentMock.currentCategories$ = currentCategories$;
+  });
+
+  const renderElement = () =>
+    render(
+      <SettingContext.Provider value={settingsContextValueMock}>
+        <StoryCategoriesSelector viewComponent={storyCategoriesViewComponentMock} />
+      </SettingContext.Provider>,
+    );
+
+  test('Should render with all elements', () => {
+    renderElement();
+    act(() => currentCategories$.next(expectedCategories));
+
+    expect(screen.getByText(translations.settingCategories)).toBeInTheDocument();
+    checkExpectedCategoriesChips();
   });
 });
